350bedb52c9acec6cc1b17936754c03e09bd0804
Roger Parent <pengytheduckwin@gmail.com>
2018-02-04 13:50:55 -0500

    Make the extension roughly functional

--- lib/middleman-grouper/extension.rb
+++ lib/middleman-grouper/extension.rb
@@ -4,7 +4,7 @@ require 'middleman-core'
 # Extension namespace
 class MiddlemanGrouper < ::Middleman::Extension
   option :name, nil, "A unique string to indentify a group among multiple groups."
-  option :scope, "/", "The folder relative to source that this group should pull resources from."
+  option :scope, nil, "The folder relative to source that this group should pull resources from."
   option :callback, nil, "A function to be called on each resource after adding to the group."
 
   def initialize(app, options_hash={}, &block)
@@ -16,6 +16,13 @@ class MiddlemanGrouper < ::Middleman::Extension
 
     # set up your extension
     # puts options.my_option
+    raise "MiddlemanGrouper needs a scope!" unless options.scope
+    options.name = options.name.to_sym
+    (@resources ||= {})[options.name] ||= {}
+  end
+
+  def scoped_resources
+    @resources[options.name]
   end
 
   def after_configuration
@@ -24,16 +31,39 @@ class MiddlemanGrouper < ::Middleman::Extension
 
   # A Sitemap Manipulator
   def manipulate_resource_list(resources)
-    binding.pry
     resources.each do |resource|
-      
+      if resource.path.start_with? options.scope
+        scoped_key = resource.normalized_path[options.scope.length .. -1]
+        scoped_resources[scoped_key] = resource
+      end
     end
-    binding.pry
     resources
   end
+
+  def group(name = nil)
+    if !name && @resources.count == 1
+      return GroupController.new(@resources.first[1])
+    end
+    return GroupController.new(@resources[name])
+  end
+  expose_to_template :group
 
   # helpers do
   #   def a_helper
   #   end
   # end
+
+  class GroupController
+    def initialize(resources)
+      @resources = resources
+    end
+
+    def all
+      return @resources.values
+    end
+
+    def find(name)
+      return @resources[name]
+    end
+  end
 end
